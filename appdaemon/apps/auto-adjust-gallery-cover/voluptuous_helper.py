13f8be8040e4cc846e0b9a03c40afa41b4a97436
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-27 08:29:24 +0200

    Uploaded AppDaemon scripts

---
+++ appdaemon/apps/auto-adjust-gallery-cover/voluptuous_helper.py
@@ -0,0 +1,39 @@
+import appdaemon.plugins.hass.hassapi as hass
+from typing import Any, Sequence, TypeVar, Union
+from datetime import datetime, time
+import voluptuous as vol
+
+
+def time(value: Any) -> time:
+    try:
+        return datetime.strptime(value, '%H:%M').time()
+    except ValueError:
+        raise vol.Invalid(f"Invalid time: {value}")
+
+
+def entity_id(value: Any) -> str:
+    value = str(value).lower()
+    if '.' in value:
+        return value
+    raise vol.Invalid(f"Invalid entity-id: {value}")
+
+
+def ensure_list(value):
+    if isinstance(value, list):
+        return value
+    elif value:
+        return [value]
+    return []
+
+
+class existing_entity_id(object):
+    def __init__(self, hass):
+        self._hass = hass
+
+    def __call__(self, value: Any) -> str:
+        value = str(value).lower()
+        if '.' not in value:
+            raise vol.Invalid(f'Invalid entity-id: {value}')
+        if not self._hass.entity_exists(value):
+            raise vol.Invalid(f'Entity-id {value} does not exist')
+        return value
